1a2b7663ddf27b85aab9196cdc5b2660112e77e2
Илья Гусев <mail@igusev.ru>
2015-06-26 20:08:02 +0300

    added PhotoSize type

---
+++ src/Types/PhotoSize.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tgbot\Api\Types;
+
+/**
+ * Class PhotoSize
+ * This object represents one size of a photo or a file / sticker thumbnail.
+ *
+ * @package tgbot\Api\Types
+ */
+class PhotoSize
+{
+    /**
+     * Unique identifier for this file
+     *
+     * @var string
+     */
+    protected $file_id;
+
+    /**
+     * Photo width
+     *
+     * @var int
+     */
+    protected $width;
+
+    /**
+     * Photo height
+     *
+     * @var int
+     */
+    protected $height;
+
+    /**
+     * Optional. File size
+     *
+     * @var int
+     */
+    protected $fileSize;
+}
