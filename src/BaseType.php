9afd9d090a3931a06d2442806af65a3dd84add0c
Илья Гусев <mail@igusev.ru>
2015-06-26 21:35:30 +0300

    abstract base type for types

---
+++ src/BaseType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tgbot\Api;
+
+
+abstract class BaseType
+{
+    function toCamelCase($string, $capitalizeFirstCharacter = false)
+    {
+        $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+
+        if (!$capitalizeFirstCharacter) {
+            $str[0] = lcfirst($str[0]);
+        }
+
+        return $str;
+    }
+}
