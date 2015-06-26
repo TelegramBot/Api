d5bab0c1bf506a578374f8a6169628db3add6f79
Илья Гусев <mail@igusev.ru>
2015-06-26 21:05:40 +0300

    added ReplyKeyboardHide type

---
+++ src/Types/ReplyKeyboardHide.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tgbot\Api\Types;
+
+/**
+ * Class ReplyKeyboardHide
+ * Upon receiving a message with this object, Telegram clients will hide the current custom keyboard
+ * and display the default letter-keyboard. By default, custom keyboards are displayed
+ * until a new keyboard is sent by a bot. An exception is made for one-time keyboards
+ * that are hidden immediately after the user presses a button (see \tgbot\Api\Types\ReplyKeyboardMarkup).
+ *
+ * @package tgbot\Api\Types
+ */
+class ReplyKeyboardHide
+{
+    /**
+     * Requests clients to hide the custom keyboard
+     *
+     * @var bool
+     */
+    protected $hideKeyboard;
+
+    /**
+     * Optional. Use this parameter if you want to show the keyboard to specific users only.
+     * Targets:
+     * 1) users that are @mentioned in the text of the Message object;
+     * 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+     *
+     * @var bool
+     */
+    protected $selective;
+}
