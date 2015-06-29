723bec9b190331152d517e8021e572fc098884eb
Илья Гусев <mail@igusev.ru>
2015-06-29 16:51:57 +0300

    added tests for Audio type

---
+++ tests/AudioTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace TelegramBot\Api\Test;
+
+use TelegramBot\Api\Types\Audio;
+
+class AudioTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetFileId()
+    {
+        $item = new Audio();
+        $item->setFileId('testfileId');
+        $this->assertAttributeEquals('testfileId', 'fileId', $item);
+    }
+
+    public function testGetFileId()
+    {
+        $item = new Audio();
+        $item->setFileId('testfileId');
+        $this->assertEquals('testfileId', $item->getFileId());
+    }
+
+    public function testSetDuration()
+    {
+        $item = new Audio();
+        $item->setDuration(1);
+        $this->assertAttributeEquals(1, 'duration', $item);
+    }
+
+    public function testGetDuration()
+    {
+        $item = new Audio();
+        $item->setDuration(1);
+        $this->assertEquals(1, $item->getDuration());
+    }
+
+    public function testSetFileSize()
+    {
+        $item = new Audio();
+        $item->setFileSize(5);
+        $this->assertAttributeEquals(5, 'fileSize', $item);
+    }
+
+    public function testGetFileSize()
+    {
+        $item = new Audio();
+        $item->setFileSize(6);
+        $this->assertEquals(6, $item->getFileSize());
+    }
+
+    public function testSetMimeType()
+    {
+        $item = new Audio();
+        $item->setMimeType('audio/mp3');
+        $this->assertAttributeEquals('audio/mp3', 'mimeType', $item);
+    }
+
+    public function testGetMimeType()
+    {
+        $item = new Audio();
+        $item->setMimeType('audio/mp3');
+        $this->assertEquals('audio/mp3', $item->getMimeType());
+    }
+
+    public function testFromResponse()
+    {
+        $item = Audio::fromResponse(array(
+            "file_id" => 'testFileId1',
+            'duration' => 1,
+            'mime_type' => 'audio/mp3',
+            'file_size' => 3
+        ));
+        $this->assertInstanceOf('\TelegramBot\Api\Types\Audio', $item);
+        $this->assertAttributeEquals('testFileId1', 'fileId', $item);
+        $this->assertAttributeEquals(1, 'duration', $item);
+        $this->assertAttributeEquals('audio/mp3', 'mimeType', $item);
+        $this->assertAttributeEquals(3, 'fileSize', $item);
+    }
+}
