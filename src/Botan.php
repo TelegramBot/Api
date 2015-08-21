bce1349ded09ccd87e85459d34033189ce613438
Илья Гусев <mail@igusev.ru>
2015-08-21 19:19:45 +0300

    added Botan.io tracking

---
+++ src/Botan.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace TelegramBot\Api;
+
+use TelegramBot\Api\Types\Message;
+
+class Botan
+{
+
+    /**
+     * @var string Tracker url
+     */
+    const BASE_URL = 'https://api.botan.io/track';
+
+    /**
+     * CURL object
+     *
+     * @var
+     */
+    protected $curl;
+
+
+    /**
+     * @var string Yandex AppMetrica application api_key
+     */
+    protected $token;
+
+    /**
+     * Botan constructor
+     *
+     * @param string $token
+     *
+     * @throws \Exception
+     */
+    function __construct($token)
+    {
+        if (!function_exists('curl_version')) {
+            throw new Exception('CURL not installed');
+        }
+
+        if (empty($token) || !is_string($token)) {
+            throw new InvalidArgumentException('Token should be a string');
+        }
+
+        $this->token = $token;
+        $this->curl = curl_init();
+    }
+
+    /**
+     * 
+     *
+     * @param \TelegramBot\Api\Types\Message $message
+     * @param string $eventName
+     *
+     * @throws \TelegramBot\Api\Exception
+     * @throws \TelegramBot\Api\HttpException
+     */
+    public function track(Message $message, $eventName = 'Message')
+    {
+        $uid = $message->getFrom()->getId();
+
+        $options = array(
+            CURLOPT_URL => self::BASE_URL . "?token={$this->token}&uid={$uid}&name={$eventName}",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json'
+            ],
+            CURLOPT_POSTFIELDS => $message->toJson()
+        );
+
+        curl_setopt_array($this->curl, $options);
+        $result = curl_exec($this->curl);
+
+        BotApi::curlValidate($this->curl);
+
+        if ($result['response']['status'] !== 'accepted') {
+            throw new Exception('Error Processing Request');
+        }
+    }
+
+    /**
+     * Destructor. Close curl
+     */
+    public function __destruct()
+    {
+        $this->curl && curl_close($this->curl);
+    }
+}
