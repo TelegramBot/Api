a01a5e2775901238df182dd032a436ea818778a2
Ilya Gusev <mail@igusev.ru>
2016-01-04 23:55:58 +0300

    added InlineQuery type

---
+++ src/Types/InlineQuery.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace TelegramBot\Api\Types;
+
+use TelegramBot\Api\BaseType;
+
+/**
+ * Class InlineQuery
+ * This object represents an incoming inline query.
+ * When the user sends an empty query, your bot could return some default or trending results.
+ *
+ * @package TelegramBot\Api\Types
+ */
+class InlineQuery extends BaseType
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    static protected $requiredParams = ['id', 'from', 'query', 'offset'];
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    static protected $map = [
+        'id' => true,
+        'from' => User::class,
+        'query' => true,
+        'offset' => true,
+    ];
+
+    /**
+     * Unique identifier for this query
+     *
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * Sender
+     *
+     * @var User
+     */
+    protected $from;
+
+    /**
+     * Text of the query
+     *
+     * @var string
+     */
+    protected $query;
+
+    /**
+     * Offset of the results to be returned, can be controlled by the bot
+     *
+     * @var string
+     */
+    protected $offset;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * @param User $from
+     */
+    public function setFrom($from)
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @param string $query
+     */
+    public function setQuery($query)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @param string $offset
+     */
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+}
