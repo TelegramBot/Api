43b1413bd310b8bb661c36db625278dec6c0d21a
Ilya Gusev <mail@igusev.ru>
2016-01-07 21:46:54 +0300

    more tests for InlineQueryResultArticle

--- tests/Types/Inline/InlineQueryResultArticleTest.php
+++ tests/Types/Inline/InlineQueryResultArticleTest.php
@@ -31,4 +31,69 @@ class InlineQueryResultArticleTest extends \PHPUnit_Framework_TestCase
 
         $this->assertAttributeEquals('article', 'type', $item);
     }
+
+    public function testGetType()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+
+        $this->assertEquals('article', $item->getType());
+    }
+
+    public function testSetId()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setId('id2');
+        $this->assertAttributeEquals('id2', 'id', $item);
+    }
+
+    public function testGetId()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setId('id2');
+        $this->assertEquals('id2', $item->getId());
+    }
+
+    public function testSetTitle()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setTitle('title2');
+        $this->assertAttributeEquals('title2', 'title', $item);
+    }
+
+    public function testGetTitle()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setTitle('title2');
+        $this->assertEquals('title2', $item->getTitle());
+    }
+
+    public function testSetMessageText()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setMessageText('messageText2');
+        $this->assertAttributeEquals('messageText2', 'messageText', $item);
+    }
+
+    public function testGetMessageText()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1');
+        $item->setMessageText('messageText2');
+        $this->assertEquals('messageText2', $item->getMessageText());
+    }
+
+    public function testSetParseMode()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1', 'Markdown');
+        $this->assertAttributeEquals('Markdown', 'parseMode', $item);
+        $item->setParseMode('Markdown2');
+        $this->assertAttributeEquals('Markdown2', 'parseMode', $item);
+    }
+
+    public function testGetParseMode()
+    {
+        $item = new InlineQueryResultArticle('id1', 'title1', 'messageText1', 'Markdown');
+        $item->setParseMode('Markdown2');
+        $this->assertEquals('Markdown2', $item->getParseMode());
+    }
+
 }
