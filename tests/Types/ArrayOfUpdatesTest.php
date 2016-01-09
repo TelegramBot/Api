d0be02900d1bbcd413ca4c8a4201d38b4bfdaec9
Ilya Gusev <mail@igusev.ru>
2016-01-10 00:51:29 +0300

    tests for ArrayOfUpdates

---
+++ tests/Types/ArrayOfUpdatesTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace TelegramBot\Api\Test\Types;
+
+use TelegramBot\Api\Types\ArrayOfUpdates;
+
+class ArrayOfUpdatesTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function data()
+    {
+        return [
+            // item 1
+            [
+                [
+                    [
+                        'update_id' => 123456,
+                        'message' => [
+                            'message_id' => 13948,
+                            'from' => [
+                                'id' => 123,
+                                'first_name' => 'Ilya',
+                                'last_name' => 'Gusev',
+                                'username' => 'iGusev',
+                            ],
+                            'chat' => [
+                                'id' => 123,
+                                'type' => 'private',
+                                'first_name' => 'Ilya',
+                                'last_name' => 'Gusev',
+                                'username' => 'iGusev',
+                            ],
+                            'date' => 1440169809,
+                            'text' => 'testText',
+                        ],
+
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider data
+     */
+    public function testFromResponse($data)
+    {
+        $items = ArrayOfUpdates::fromResponse($data);
+
+        $this->assertInternalType('array', $items);
+
+        foreach($items as $item) {
+            $this->assertInstanceOf('\TelegramBot\Api\Types\Update', $item);
+        }
+    }
+}
