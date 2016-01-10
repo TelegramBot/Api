06f45fd64063be38f67f06dec865fd4e1decf37d
Ilya Gusev <mail@igusev.ru>
2016-01-10 23:02:32 +0300

    added tests for Client

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,10 +5,14 @@ namespace TelegramBot\Api\Test;
 
 use TelegramBot\Api\BotApi;
 use TelegramBot\Api\Client;
+use TelegramBot\Api\Types\Message;
 use TelegramBot\Api\Types\Update;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected $test;
+
     public function data()
     {
         return [
@@ -34,6 +38,61 @@ class ClientTest extends \PHPUnit_Framework_TestCase
                         'text' => 'testText',
                     ],
                 ]),
+                'testText',
+                null,
+                null
+            ],
+            [
+                Update::fromResponse([
+                    'update_id' => 123456,
+                    'message' => [
+                        'message_id' => 13948,
+                        'from' => [
+                            'id' => 123,
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'chat' => [
+                            'id' => 123,
+                            'type' => 'private',
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'date' => 1440169809,
+                        'text' => '/testcommand',
+                    ],
+                ]),
+                'testcommand',
+                null,
+                null
+            ],
+            [
+                Update::fromResponse([
+                    'update_id' => 123456,
+                    'message' => [
+                        'message_id' => 13948,
+                        'from' => [
+                            'id' => 123,
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'chat' => [
+                            'id' => 123,
+                            'type' => 'private',
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'date' => 1440169809,
+                        'text' => '/testcommand with attrs',
+                    ],
+                ]),
+                'testcommand',
+                'with',
+                'attrs'
             ],
             [
                 Update::fromResponse([
@@ -51,6 +110,9 @@ class ClientTest extends \PHPUnit_Framework_TestCase
                     ],
 
                 ]),
+                'testcommand',
+                null,
+                null
             ],
         ];
     }
@@ -111,5 +173,101 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($item, $item->on(function () {
             return true;
         }));
+    }
+
+    /**
+     * @param Update $update
+     * @param string $command
+     *
+     * @dataProvider data
+     */
+    public function testGetChecker($update, $command)
+    {
+        $reflectionMethod = new \ReflectionMethod('TelegramBot\Api\Client', 'getChecker');
+        $reflectionMethod->setAccessible(true);
+
+        $result = $reflectionMethod->invoke(null, $command);
+
+        $this->assertInstanceOf('\Closure', $result);
+
+        preg_match(Client::REGEXP, $update->getMessage() ? $update->getMessage()->getText() : '', $matches);
+
+        $expected = !empty($matches) && $matches[1] == $command;
+
+        $this->assertEquals($expected, call_user_func($result, $update));
+
+    }
+
+    /**
+     * @param Update $update
+     *
+     * @dataProvider data
+     */
+    public function testHandle($update)
+    {
+        $item = new Client('testToken');
+
+        $mockedEventCollection = $this->getMockBuilder('\TelegramBot\Api\Events\EventCollection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockedEventCollection->expects($this->exactly(2))->method('handle');
+
+        $reflection = new \ReflectionClass($item);
+        $reflectionProperty = $reflection->getProperty('events');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($item, $mockedEventCollection);
+        $reflectionProperty->setAccessible(false);
+
+        $item->handle([$update, $update]);
+
+    }
+
+    /**
+     * @param Update $update
+     *
+     * @dataProvider data
+     */
+    public function testGetEvent($update, $command, $attr1, $attr2)
+    {
+        $reflectionMethod = new \ReflectionMethod('TelegramBot\Api\Client', 'getEvent');
+        $reflectionMethod->setAccessible(true);
+        global $test;
+
+        $test = 1;
+
+        $action = function (Message $message) {
+            global $test;
+            $test = 2;
+
+            return true;
+        };
+
+        if($attr1 && $attr2) {
+            $action = function (Message $message, $attr1, $attr2) {
+                global $test;
+                $test = 2;
+
+                return true;
+            };
+        }
+
+        $action->bind($action, $this);
+
+        $result = $reflectionMethod->invoke(null, $action);
+
+        $this->assertInstanceOf('\Closure', $result);
+
+        $mustBeCalled = !is_null($update->getMessage());
+
+        $this->assertEquals(!$mustBeCalled, call_user_func($result, $update));
+
+        if ($mustBeCalled) {
+            $this->assertEquals(2, $test);
+        } else {
+            $this->assertEquals(1, $test);
+        }
+
+
     }
 }
