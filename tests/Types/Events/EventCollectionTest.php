0388924fc09af09b51d41f42bde23bc6130e4300
Ilya Gusev <mail@igusev.ru>
2016-01-10 05:51:26 +0300

    Added tests for TelegramBot\Api\Events\EventCollection

--- tests/Types/Events/EventCollectionTest.php
+++ tests/Types/Events/EventCollectionTest.php
@@ -1,15 +1,176 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: iGusev
- * Date: 10/01/16
- * Time: 05:07
- */
 
 namespace TelegramBot\Api\Test\Types\Events;
 
+use TelegramBot\Api\Client;
+use TelegramBot\Api\Events\EventCollection;
+use TelegramBot\Api\Types\Update;
 
 class EventCollectionTest extends \PHPUnit_Framework_TestCase
 {
+    public function data()
+    {
+        return [
+            [
+                function ($update) {
+                    return false;
+                },
+                function ($update) {
+                    return true;
+                },
+                Update::fromResponse([
+                    'update_id' => 123456,
+                    'message' => [
+                        'message_id' => 13948,
+                        'from' => [
+                            'id' => 123,
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'chat' => [
+                            'id' => 123,
+                            'type' => 'private',
+                            'first_name' => 'Ilya',
+                            'last_name' => 'Gusev',
+                            'username' => 'iGusev',
+                        ],
+                        'date' => 1440169809,
+                        'text' => 'testText',
+                    ],
+                ]),
+            ],
+        ];
+    }
+
+    public function testConstructor1()
+    {
+        $item = new EventCollection();
+
+        $this->assertAttributeEmpty('tracker', $item);
+        $this->assertAttributeEmpty('events', $item);
+    }
+
+    public function testConstructor2()
+    {
+        $item = new EventCollection('testToken');
+
+        $this->assertAttributeInstanceOf('\TelegramBot\Api\Botan', 'tracker', $item);
+        $this->assertAttributeEmpty('events', $item);
+    }
+
+    /**
+     * @param \Closure $action
+     * @param \Closure $checker
+     *
+     * @dataProvider data
+     */
+    public function testAdd1($action, $checker)
+    {
+        $item = new EventCollection();
+        $item->add($action, $checker);
+
+        $reflection = new \ReflectionClass($item);
+        $reflectionProperty = $reflection->getProperty('events');
+        $reflectionProperty->setAccessible(true);
+        $innerItem = $reflectionProperty->getValue($item);
+        $reflectionProperty->setAccessible(false);
+
+        $this->assertAttributeInternalType('array', 'events', $item);
+        /* @var \TelegramBot\Api\Events\Event $event */
+        foreach($innerItem as $event) {
+            $this->assertInstanceOf('\TelegramBot\Api\Events\Event', $event);
+        }
+    }
+
+    /**
+     * @param \Closure $action
+     *
+     * @dataProvider data
+     */
+    public function testAdd2($action)
+    {
+        $item = new EventCollection();
+        $item->add($action);
+
+        $reflection = new \ReflectionClass($item);
+        $reflectionProperty = $reflection->getProperty('events');
+        $reflectionProperty->setAccessible(true);
+        $innerItem = $reflectionProperty->getValue($item);
+        $reflectionProperty->setAccessible(false);
+
+        $this->assertAttributeInternalType('array', 'events', $item);
+        /* @var \TelegramBot\Api\Events\Event $event */
+        foreach($innerItem as $event) {
+            $this->assertInstanceOf('\TelegramBot\Api\Events\Event', $event);
+        }
+    }
+
+    /**
+     * @param \Closure $action
+     * @param \Closure $checker
+     * @param Update $update
+     *
+     * @dataProvider data
+     */
+    public function testHandle1($action, $checker, $update) {
+        $item = new EventCollection('testToken');
+        $name = 'test';
+        $item->add($action, function ($update) use ($name) {
+            return true;
+        });
+
+        $mockedTracker = $this->getMockBuilder('\TelegramBot\Api\Botan')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Configure the stub.
+
+        $mockedTracker->expects($this->once())->method('track')->willReturn(null);
+
+        $reflection = new \ReflectionClass($item);
+        $reflectionProperty = $reflection->getProperty('tracker');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($item, $mockedTracker);
+        $reflectionProperty->setAccessible(false);
+
+        $item->handle($update);
+    }
+
+    /**
+     * @param \Closure $action
+     * @param \Closure $checker
+     * @param Update $update
+     *
+     * @dataProvider data
+     */
+    public function testHandle2($action, $checker, $update) {
+        $item = new EventCollection();
+        $name = 'test';
+        $item->add($action, function ($update) use ($name) {
+            return true;
+        });
+
+        $mockedTracker = $this->getMockBuilder('\TelegramBot\Api\Botan')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockedEvent = $this->getMockBuilder('\TelegramBot\Api\Events\Event')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Configure the stub.
+        $mockedTracker->expects($this->exactly(0))->method('track')->willReturn(null);
+        $mockedEvent->expects($this->once())->method('executeChecker')->willReturn(true);
+        $mockedEvent->expects($this->once())->method('executeAction')->willReturn(true);
+
+        $reflection = new \ReflectionClass($item);
+        $reflectionProperty = $reflection->getProperty('events');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($item, [$mockedEvent]);
+        $reflectionProperty->setAccessible(false);
+
+        $item->handle($update);
+    }
 
 }
