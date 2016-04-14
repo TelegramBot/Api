26931113d5c04ecd2760aa2f625ee28124378544
Ilya Gusev <mail@igusev.ru>
2016-04-14 15:44:55 +0300

    added InputLocationMessageContent

---
+++ src/Types/Inline/InputLocationMessageContent.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iGusev
+ * Date: 14/04/16
+ * Time: 15:41
+ */
+
+namespace TelegramBot\Api\Types\Inline;
+
+use TelegramBot\Api\BaseType;
+use TelegramBot\Api\TypeInterface;
+
+/**
+ * Class InputLocationMessageContent
+ * @see https://core.telegram.org/bots/api#inputlocationmessagecontent
+ *
+ * Represents the content of a location message to be sent as the result of an inline query.
+ *
+ * @package TelegramBot\Api\Types\Inline
+ */
+class InputLocationMessageContent extends BaseType implements TypeInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    static protected $requiredParams = ['latitude', 'longitude'];
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var array
+     */
+    static protected $map = [
+        'latitude' => true,
+        'longitude' => true
+    ];
+
+    /**
+     * Latitude of the location in degrees
+     *
+     * @var float
+     */
+    protected $latitude;
+
+    /**
+     * Longitude of the location in degrees
+     *
+     * @var float
+     */
+    protected $longitude;
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+}
