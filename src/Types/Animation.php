29a304bb040af4511050522b1ade117eccd115ad
Ilya Gusev <mail@igusev.ru>
2019-12-16 08:52:33 +0300

    actualize animation type

--- src/Types/Animation.php
+++ src/Types/Animation.php
@@ -8,7 +8,7 @@ use TelegramBot\Api\TypeInterface;
 
 /**
  * Class Animation
- * This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound).
+ * This object represents an animation file (GIF or H.264/MPEG-4 AVC video without sound)
  *
  * @package TelegramBot\Api\Types
  */
@@ -38,7 +38,7 @@ class Animation extends BaseType implements TypeInterface
     ];
 
     /**
-     * Unique file identifier
+     * Identifier for this file
      *
      * @var string
      */
@@ -66,182 +66,182 @@ class Animation extends BaseType implements TypeInterface
     protected $duration;
 
     /**
-     * Video thumbnail
+     * Optional. Animation thumbnail as defined by sender
      *
-     * @var PhotoSize
+     * @var PhotoSize|null
      */
     protected $thumb;
 
     /**
-     * Optional. Animation thumbnail as defined by sender
+     * Optional. Original animation filename as defined by sender
      *
      * @var PhotoSize
      */
     protected $fileName;
 
     /**
-     * Optional. Mime type of a file as defined by sender
+     * Optional. MIME type of the file as defined by sender
      *
-     * @var string
+     * @var string|null
      */
     protected $mimeType;
 
     /**
      * Optional. File size
      *
-     * @var int
+     * @var int|null
      */
     protected $fileSize;
 
     /**
-     * @return int
-     */
-    public function getDuration()
-    {
-        return $this->duration;
-    }
-
-    /**
-     * @param int $duration
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setDuration($duration)
-    {
-        if (is_integer($duration)) {
-            $this->duration = $duration;
-        } else {
-            throw new InvalidArgumentException();
-        }
-    }
-
-    /**
      * @return string
      */
-    public function getFileId()
+    public function getFileId(): string
     {
         return $this->fileId;
     }
 
     /**
      * @param string $fileId
+     * @return Animation
      */
-    public function setFileId($fileId)
+    public function setFileId(string $fileId): Animation
     {
         $this->fileId = $fileId;
+        
+        return $this;
     }
 
     /**
      * @return int
      */
-    public function getFileSize()
-    {
-        return $this->fileSize;
-    }
-
-    /**
-     * @param int $fileSize
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setFileSize($fileSize)
-    {
-        if (is_integer($fileSize)) {
-            $this->fileSize = $fileSize;
-        } else {
-            throw new InvalidArgumentException();
-        }
-    }
-
-    /**
-     * @return int
-     */
-    public function getHeight()
-    {
-        return $this->height;
-    }
-
-    /**
-     * @param int $height
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setHeight($height)
-    {
-        if (is_integer($height)) {
-            $this->height = $height;
-        } else {
-            throw new InvalidArgumentException();
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function getMimeType()
-    {
-        return $this->mimeType;
-    }
-
-    /**
-     * @param string $mimeType
-     */
-    public function setMimeType($mimeType)
-    {
-        $this->mimeType = $mimeType;
-    }
-
-    /**
-     * @return PhotoSize
-     */
-    public function getThumb()
-    {
-        return $this->thumb;
-    }
-
-    /**
-     * @param PhotoSize $thumb
-     */
-    public function setThumb(PhotoSize $thumb)
-    {
-        $this->thumb = $thumb;
-    }
-
-    /**
-     * @return string $fileName
-     */
-    public function getFileName()
-    {
-        return $this->fileName;
-    }
-
-    /**
-     * @param string $fileName
-     */
-    public function setFileName($fileName)
-    {
-        $this->fileName = $fileName;
-    }
-
-    /**
-     * @return int
-     */
-    public function getWidth()
+    public function getWidth(): int
     {
         return $this->width;
     }
 
     /**
      * @param int $width
-     *
-     * @throws InvalidArgumentException
+     * @return Animation
      */
-    public function setWidth($width)
+    public function setWidth(int $width): Animation
     {
-        if (is_integer($width)) {
-            $this->width = $width;
-        } else {
-            throw new InvalidArgumentException();
-        }
+        $this->width = $width;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param int $height
+     * @return Animation
+     */
+    public function setHeight(int $height): Animation
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param int $duration
+     * @return Animation
+     */
+    public function setDuration(int $duration): Animation
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * @return PhotoSize|null
+     */
+    public function getThumb(): ?PhotoSize
+    {
+        return $this->thumb;
+    }
+
+    /**
+     * @param PhotoSize $thumb
+     * @return Animation
+     */
+    public function setThumb(PhotoSize $thumb): Animation
+    {
+        $this->thumb = $thumb;
+
+        return $this;
+    }
+
+    /**
+     * @return PhotoSize
+     */
+    public function getFileName(): PhotoSize
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @param PhotoSize $fileName
+     * @return Animation
+     */
+    public function setFileName(PhotoSize $fileName): Animation
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMimeType(): ?string
+    {
+        return $this->mimeType;
+    }
+
+    /**
+     * @param string|null $mimeType
+     * @return Animation
+     */
+    public function setMimeType(string $mimeType): Animation
+    {
+        $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getFileSize(): ?int
+    {
+        return $this->fileSize;
+    }
+
+    /**
+     * @param int $fileSize
+     * @return Animation
+     */
+    public function setFileSize(int $fileSize): Animation
+    {
+        $this->fileSize = $fileSize;
+
+        return $this;
     }
 }
